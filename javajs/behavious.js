531bada370deddea0f7eafcd708328876f20ae26
noemiepham <huynhtieuquy@gmail.com>
2022-02-18 05:13:50 +0100

    them photo

---
+++ javajs/behavious.js
@@ -0,0 +1,12 @@
+﻿  $(function(){
+     vitricuaedu = $('#khoiedu').offset().top;
+     console.log(vitricuaedu);
+ $(".xuong").click(function(){
+     $('body,html').animate({scrollTop:vitricuaedu})
+ }) 
+ }); 
+  
+ 
+
+ var time = new Date();
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
